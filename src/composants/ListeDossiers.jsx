61c99dd082c406772131dfbf073814eeaaf81017
N3ris <tommy.lorenzo1517@gmail.com>
2021-03-30 23:03:06 -0400

    test2

--- src/composants/ListeDossiers.jsx
+++ src/composants/ListeDossiers.jsx
@@ -1,9 +1,46 @@
 import './ListeDossiers.scss';
 import Dossier from './Dossier';
 import * as crudDossiers from '../services/crud-dossiers';
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 import { useState, useEffect } from 'react';
 
+const useStyles = makeStyles((theme) => ({
+  button: {
+    display: "block",
+    marginTop: theme.spacing(2)
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  }
+}));
+
 export default function ListeDossiers({utilisateur, etatDossiers}) {
+  const classes = useStyles();
+  const [affichage, setAffichage] =useState("");
+  const [open, setOpen] = useState(false);
+
+  async function handleChange(idd) {
+    // On fait appel à la méthode supprimer de notre code d'interaction avec Firestore
+    crudDossiers.supprimer(utilisateur.uid, idd).then(
+      () => {
+        const tempDossiers = [...dossiers]; // copier le tableau des dossiers existants
+        const dossiersRestants = tempDossiers.filter((elt) => elt.id!==idd); // filtrer pour garder tous les dossiers sauf celui qu'on a demandé de supprimer
+        setDossiers(dossiersRestants); // Muter l'état pour forcer le réaffichage du composant
+      }).catch(erreur => console.log('Échec de la suppression - Firestore a répondu :', erreur.message));
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
   // État des dossiers (vient du composant Appli)
   const [dossiers, setDossiers] = etatDossiers;
 
@@ -37,9 +74,31 @@ export default function ListeDossiers({utilisateur, etatDossiers}) {
   
   return (
     <>
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-controlled-open-select-label"> trier l’affichage</InputLabel>
+        <Select
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          open={open}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          value={affichage}
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
     <ul className="ListeDossiers">
       {
         (dossiers.length > 0) ?
+        
           dossiers.map(dossier => <li key={dossier.id}><Dossier {...dossier} gererSupprimer={gererSupprimer} /></li>)
         :
           <li className="msgAucunDossier">
