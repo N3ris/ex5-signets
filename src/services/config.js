ef9a58012fca4683620985ca72d219bea7628deb
N3ris <tommy.lorenzo1517@gmail.com>
2021-03-30 20:56:22 -0400

    test

---
+++ src/services/config.js
@@ -0,0 +1,21 @@
+/*
+  Ce fichier contient les variables de configuration
+  reliées à Firebase et ses produits.
+*/
+
+// Configuration Firebase (mettez-y les vôtres !)
+const firebaseConfig = {
+  apiKey: "AIzaSyCHiSTo91xv4uhT04DozK_9hcLNs9QEgKE",
+  authDomain: "exercice-5.firebaseapp.com",
+  projectId: "exercice-5",
+  storageBucket: "exercice-5.appspot.com",
+  messagingSenderId: "1084937730315",
+  appId: "1:1084937730315:web:38482a5aac7ee18ae8ca0e",
+  measurementId: "G-GQ2V9ZZBW6"
+};
+export default firebaseConfig;
+
+// Nom de la collection principale
+export const utilRef = "utilisateurs-ex5";
+// Nom de la sous-collection
+export const dossRef = "dossiers";
